870ed7e26573c67ed365fd60b68c31e641d7f8dc
LorenBll <lorenzo.bellotta@hotmail.com>
2025-05-13 18:42:12 +0200

    first commit

---
+++ src/main/java/ecycle/ecycle/EcycleApplication.java
@@ -0,0 +1,15 @@
+package ecycle.ecycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcycleApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EcycleApplication.class, args);
+	
+	}
+
+}
---
+++ src/test/java/ecycle/ecycle/EcycleApplicationTests.java
@@ -0,0 +1,13 @@
+package ecycle.ecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
